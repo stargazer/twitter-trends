5ea6a77b6eed5a4c40197cdefb5d6fd3823ce8ab
stargazer <already.late@gmail.com>
2011-12-12 21:46:55 +0100

    Modified project structure

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
 	name = "twitter-trends",
 	version = "0.1",
-	packages = find_packages('src'),
-	package_dir = {'': 'src'},
+	packages = find_packages(),
 	install_requires = [
 		'setuptools',
 	],
