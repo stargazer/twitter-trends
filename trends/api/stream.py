d96451e526254091a58e9e51bc106719fd843048
stargazer <already.late@gmail.com>
2011-12-13 19:50:25 +0100

    Credentials are taken from another file

--- trends/api/stream.py
+++ trends/api/stream.py
@@ -1,27 +1,23 @@
 from trends.api import oauth
+from trends.api import credentials
 import urllib2
 import contextlib
 
 class Stream:
 	URL = 'https://stream.twitter.com/1/statuses/sample.json'
 	
-	CONSUMER_KEY = 'eU0DQBRlTtm4Ht3dJ9DDtA'
-	CONSUMER_SECRET = 'uER0UOsCbT7BwLm0la6mokP7fjfrrCVTnVU6uGPjKk'
-	TOKEN_KEY = '88961589-ZGUU9ntHNxZ3FZ0JVf6R1dqA8GXeA1hePA90EakFE'
-	TOKEN_SECRET = 'QdEei39k2CtzYomiu3y97RYCQ5FRYnGgvmS9TxFHm8'
-
 	@classmethod
 	def get(cls):
 		"""
 		Returns an open socket that listens to the Twitter streaming API.
 		"""
 		consumer = oauth.OAuthConsumer(
-			cls.CONSUMER_KEY,
-			cls.CONSUMER_SECRET,
+			credentials.CONSUMER_KEY,
+			credentials.CONSUMER_SECRET,
 		)
 		token = oauth.OAuthToken(
-			cls.TOKEN_KEY,
-			cls.TOKEN_SECRET,
+			credentials.TOKEN_KEY,
+			credentials.TOKEN_SECRET,
 		)
 
 		oauth_request = oauth.OAuthRequest.from_consumer_and_token(
